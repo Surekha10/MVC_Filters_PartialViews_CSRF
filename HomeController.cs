2585dc51e67b6d1e2ae8824aa0e4aac14b77a155
Surekha10 <45581026+Surekha10@users.noreply.github.com>
2019-01-04 15:06:11 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Filters_PartialView_CSRF.Controllers
+{
+    [HandleError(View ="Error")]//Exception Filter
+    public class HomeController : Controller
+    {
+        [OutputCache(Duration =30)]//Result Filter
+        public ActionResult GetDetails()
+        {
+            ViewBag.msg = DateTime.Now.ToString();
+            return View();
+        }
+        [HandleError(ExceptionType=typeof(DivideByZeroException),View ="MyError")]
+        public ActionResult GetUpdate()
+        {
+            int x = 100;
+            int y = 0;
+            int total = x / y;
+            return View();
+        }
+    }
+}
